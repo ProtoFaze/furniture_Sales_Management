5a20406f71729e623bc3b2d276ff2766317dea63
Damon <110209956+ProtoFaze@users.noreply.github.com>
2023-12-19 14:28:24 +0800

    reused old login function, refactored remember me feature to use JSON objects added write function for singular json objects

--- furniture_Sales_Management/src/Classes/File.java
+++ furniture_Sales_Management/src/Classes/File.java
@@ -57,11 +57,11 @@ public class File {
         List<Admin> admins = new ArrayList<>();
 
         // store json in memory 
-        JsonObject UserJson = read(fileName);
-        JsonArray roomArray = UserJson.getAsJsonArray(fileName);
+        JsonObject userJson = read(fileName);
+        JsonArray UserArray = userJson.getAsJsonArray(fileName);
         
         // store admins into list then return when done
-        for (JsonElement element: roomArray){
+        for (JsonElement element: UserArray){
             JsonObject adminJson = element.getAsJsonObject();
             if ("admin".equals(adminJson.get("role").getAsString())){
                 System.out.println(adminJson);
@@ -80,11 +80,11 @@ public class File {
         List<Officer> officers = new ArrayList<>();
 
         // store json in memory 
-        JsonObject UserJson = read(fileName);
-        JsonArray roomArray = UserJson.getAsJsonArray(fileName);
+        JsonObject userJson = read(fileName);
+        JsonArray userArray = userJson.getAsJsonArray(fileName);
         
         // store admins into list then return when done
-        for (JsonElement element: roomArray){
+        for (JsonElement element: userArray){
             JsonObject officerJson = element.getAsJsonObject();
             if ("officer".equals(officerJson.get("role").getAsString())){
                 System.out.println(officerJson);
@@ -96,22 +96,40 @@ public class File {
         return officers;
     }
     /**
-     *
+     * For files containing multiple records, like users, sales
      * V0.1 rewrites the entire file to edit//add info, usable for 1 element containing array only
      * @param fileName name of the file to be edited
-     * @param content (JSON object) the entire file content saved within main after it has been updated
+     * @param contents (JSON object) the entire file contents saved within main after it has been updated
      * @return status of edit operation
      */
-    public static String write(String fileName, JsonArray content){
+    public static String write(String fileName, JsonArray contents){
         String file = SRCPATH+fileName+EXT;
         try(PrintWriter outputFile = new PrintWriter(new FileWriter(file, false))){
             outputFile.println("{\""+fileName+"\":[");
-            for(JsonElement element: content){
+            for(JsonElement element: contents){
                 //print out each record
                 JsonObject record = element.getAsJsonObject();
                 outputFile.println(record);
             }
             outputFile.println("]}");
+            return "Success";
+        }catch (IOException ex){
+            return ex.toString();
+        }
+    }
+    /**
+     * For files containing only 1 record, like save or config files
+     * V0.1 rewrites the entire file to edit//add info, usable for 1 element containing array only
+     * @param fileName name of the file to be edited
+     * @param content (JSON object) the entire file contents saved within main after it has been updated
+     * @return status of edit operation
+     */
+    public static String write(String fileName, JsonObject content){
+        String file = SRCPATH+fileName+EXT;
+        try(PrintWriter outputFile = new PrintWriter(new FileWriter(file, false))){
+            outputFile.println("{\""+fileName+"\":");
+            outputFile.println(content);
+            outputFile.println("}");
             return "Success";
         }catch (IOException ex){
             return ex.toString();
--- furniture_Sales_Management/src/furniture_sales_management/Furniture_Sales_Management.java
+++ furniture_Sales_Management/src/furniture_sales_management/Furniture_Sales_Management.java
@@ -4,24 +4,17 @@
  */
 package furniture_sales_management;
 
-import Classes.Admin;
 import Classes.File;
-import java.util.List;
+import Interface.Login;
 //import com.toedter.calendar.JDateChooser;
 
 public class Furniture_Sales_Management {
-    static List<Admin> admins ;
-    
-    /**
+        /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//        LoginPage login = new LoginPage(File.read("room"));
-           admins = File.readAdmins();
-           System.out.println(admins.size());
-           for (Admin admin : admins){
-               System.out.print(admin.getMail());
-           }
+        Login login = new Login(File.readAdmins());
+        login.setVisible(true);
 //        JDateChooser start = new JDateChooser();
 //        System.out.print(start.getDate());
         
